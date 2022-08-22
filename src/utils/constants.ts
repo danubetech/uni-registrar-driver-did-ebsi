de75d096ee6403cab1cde61f3f0e75728c9c35de
azuzi <azeem.will@gmail.com>
2022-08-22 15:21:31 +0200

    chore: Constatnst values added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
 export const CONTEXT_W3C_DID = "https://www.w3.org/ns/did/v1";
-export const CONTEXT_W3C_SEC_SECP256K1 =
+export const CONTEXT_W3C_SEC =
   "https://w3id.org/security/suites/secp256k1-2019/v1";
-export const CONTEXT_W3C_SEC_JWS="https://w3id.org/security/suites/jws-2020/v1";
+  export const CONTEXT_W3C_SEC_JWS2020 ="https://w3id.org/security/suites/jws-2020/v1"
 export const CONTEXT_W3C_VC = "https://www.w3.org/2018/credentials/v1";
-export const OIDC_ISSUE = "https://self-issued.me";
+export const OIDC_ISSUE = "https://self-issued.me/v2";
 export const VERIFIABLE_PRESENTATION = "VerifiablePresentation";
 export const ECDSA_SECP_256_K1_SIGNATURE_2019 = "EcdsaSecp256k1Signature2019";
 export const ECDSA_SECP_256_K1_VERIFICATION_KEY_2019 =
@@ -11,3 +11,7 @@ export const ECDSA_SECP_256_K1_VERIFICATION_KEY_2019 =
 export const JSON_WEB_Key_2020 = "JsonWebKey2020";
 export const ES256K = "ES256K";
 export const ASSERTION_METHOD = "assertionMethod";
+export const AUTHENTICATION = "authentication";
+export const VERIFICATION_METHOD ="verificationMethod";
+export const CONTEXT ="@context";
+export const KEY_1 = "#key-1";
